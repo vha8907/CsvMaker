f0f14523e7104137a1bcdd5ede1bcd546c7bec78
32311134 <viniciusha@gmail.com>
2019-10-10 14:54:43 -0300

    program properly commited

--- csvmaker.py
+++ csvmaker.py
@@ -4,15 +4,26 @@
 ### for other programs.
 
 import csv, random
+from string import ascii_uppercase as uppercase
 
-column_Number = 10
-active_Columns = []
-possibleNames = list(ascii_uppercase)
-for i in range(column_Number):
-    active_Columns.append(possibleNames[i])
+def makeCSV(col_N, row_N, range_Start, range_End):
 
-print(active_Columns)
+    # generator of columns (using the alphabet in uppercase as name reference)
+    columns = col_N
+    column_Reference = list(uppercase)
+    column_Names = []
+    for n in range(columns):
+        column_Names.append(column_Reference[n])
 
-with open("testfile.csv", 'w', newline='') as f:
-    csv_w = csv.DictWriter(f, fieldnames=['Numbers', 'MoreNumbers'])
-    csv_w.writeheader()
+    # generates random numeric content to fill in the rows
+    rows = row_N
+    row_Data = []
+    for n in range(rows):
+        row_Data.append([random.randint(range_Start, range_End) for x in range(columns)])
+
+    # creates a csv file in the present working directory:
+    with open("testfile.csv", 'w') as f:
+        csv_w = csv.writer(f)
+        csv_w.writerow(column_Names)
+        for r in row_Data:
+            csv_w.writerow(r)
